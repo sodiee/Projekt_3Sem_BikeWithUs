5d3a3b0c4c134b2def55285faa5c861cd20878b6
John Doe <mathi.carlsen@gmail.com>
2023-11-14 10:06:34 +0100

    Første commit

---
+++ Storage/DB.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrsey-IX0FhSucNOCKDrEn8ofFzbwn_7o",
+  authDomain: "bikewithus-7e8f0.firebaseapp.com",
+  projectId: "bikewithus-7e8f0",
+  storageBucket: "bikewithus-7e8f0.appspot.com",
+  messagingSenderId: "285890761175",
+  appId: "1:285890761175:web:ece7f116b6154b440ebb92"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
