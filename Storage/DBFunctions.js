6b0c4b700f9e271b5409d867a694147c33ba7f00
sodiee <mathi.carlsen@gmail.com>
2023-11-15 10:44:33 +0100

    "test" af funktion i dbfunctions

--- Storage/DBFunctions.js
+++ Storage/DBFunctions.js
@@ -25,6 +25,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+//CustomerCollection
 const CustomersCollection = collection(db, 'Customers')
 
 const getCustomersDB = async () => {
