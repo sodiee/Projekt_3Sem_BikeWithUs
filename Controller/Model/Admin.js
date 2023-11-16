8a5573aa2b198dea69c3fdcc5dc161043f16918a
lukeKING69 <eaaluah@students.eaaa.dk>
2023-11-16 10:03:43 +0100

    Ændring af klasser

--- Controller/Model/Admin.js
+++ Controller/Model/Admin.js
@@ -1,11 +1,2 @@
 import DBFunctions from '../../Storage/DBFunctions.js';
 
-function Admin(username, password, adminID) {
-    this.username = username;
-    this.password = password;
-    this.adminID = adminID;
-}
-
-function isAdmin(adminID) {
-
-}
