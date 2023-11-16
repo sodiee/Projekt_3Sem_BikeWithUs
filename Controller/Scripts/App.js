fb89851ffe4a639f5c7a138650949a59c70206b0
GloriousHyDra <lavrinenkom03@gmail.com>
2023-11-16 10:41:11 +0100

    idk prøver at starte server'en

--- Controller/Scripts/App.js
+++ Controller/Scripts/App.js
@@ -1,4 +1,4 @@
-import DBFunctions from "./Storage/DBFunctions";
+import DBFunctions from '../../Storage/DBFunctions.js';
 const express = require('express')
 const app = express()
 app.set('view engine', 'pug')
