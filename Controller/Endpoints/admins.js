ffa2e0a5abeab621da6fcb031f436f0a8effcfcc
lukeKING69 <eaaluah@students.eaaa.dk>
2023-11-21 09:23:43 +0100

    Udvidelse af admin

--- Controller/Endpoints/admins.js
+++ Controller/Endpoints/admins.js
@@ -1,28 +1,18 @@
 import express from 'express';
 const adminRouter = express.Router();
-import controller from '../Model/Journey.js';
+import controllerJourney from '../Model/Journey.js';
+import controllerDriver from '../Model/Driver.js';
+import controllerCustomer from '../Model/Customer.js';
+import controllerAdmin from '../Model/Admin.js'
 
-adminRouter.get('/Kundeinformation', async (req, res) => {
-       
+// ----------------------------
+// admin-ENDPOINTS for oversigt|
+// ----------------------------
 
-}) 
-
-adminRouter.get('/Journey/Edit/:id', async (req, res) => {
-    try {
-        const journeyId = req.params.id;
-        const journey = await controller.getJourney(journeyId);
-        
-        res.render('../GUI/views/EditJourney', { journey });
-    } catch (error) {
-        console.error('Fejl ved redigering af rejse:', error);
-        res.status(500).send('Der opstod en fejl ved redigering af rejse.');
-    }
-})
-
-adminRouter.get('/journey/Overview', async (req, res) => {
+adminRouter.get('/Journey/Overview', async (req, res) => {
     try{
     //finder alle oversigter over journeys
-    const journeys = await controller.getJourneys();
+    const journeys = await controllerJourney.getJourneys();
 
     
     res.render('../GUI/views/journeys', {journeys: journeys})
@@ -31,6 +21,67 @@ adminRouter.get('/journey/Overview', async (req, res) => {
         res.status(500).send('Der opstod en fejl ved hentning af rejser');
     }
 })
+
+adminRouter.get('/customers', async (req, res) => {
+    try {
+        // Finder alle customers
+        const customers = await controllerCustomer.getCustomers();
+        console.log(customers)
+        res.render('../GUI/views/customers', { customers: customers });
+    } catch (error) {
+        console.error('Fejl ved hentning af kunder:', error);
+        res.status(500).send('Der opstod en fejl ved hentning af kunder.');
+    }
+});
+
+adminRouter.get('/Kundeinformation/:id', async (req, res) => {
+       
+
+}) 
+
+
+// ------------------------------------
+// admin-ENDPOINTS for CRUD til drivers|
+// ------------------------------------
+
+
+// --------------------------------------
+// admin-ENDPOINTS for CRUD til customers|
+// --------------------------------------
+
+
+customerRouter.post('/Customer/Add', async (req, res) => {
+    try {
+        const { firstName, lastName, birthday, city } = req.body;
+        await controllerCustomer.addCustomer({ firstName, lastName, birthday, city });
+        
+        res.redirect('/customers'); // Redirect til en oversigtsside eller anden relevant side
+    } catch (error) {
+        console.error('Fejl ved tilføjelse af kunde:', error);
+        res.status(500).send('Der opstod en fejl ved tilføjelse af kunde.');
+    }
+});
+
+
+
+
+// ----------------------------
+// admin-ENDPOINTS for oversigt|
+// ----------------------------
+
+
+adminRouter.get('/Journey/Edit/:id', async (req, res) => {
+    try {
+        const journeyId = req.params.id;
+        const journey = await controllerJourney.getJourney(journeyId);
+        
+        res.render('../GUI/views/EditJourney', { journey });
+    } catch (error) {
+        console.error('Fejl ved redigering af rejse:', error);
+        res.status(500).send('Der opstod en fejl ved redigering af rejse.');
+    }
+})
+
 
 
 export default adminRouter;
--- Controller/Endpoints/customers.js
+++ Controller/Endpoints/customers.js
@@ -2,29 +2,9 @@ import express from 'express';
 const customerRouter = express.Router();
 import controller from '../Model/Customer.js';
 
-customerRouter.get('/customers', async (req, res) => {
-    try {
-        // Finder alle customers
-        const customers = await controller.getCustomers();
-        console.log(customers)
-        res.render('../GUI/views/customers', { customers: customers });
-    } catch (error) {
-        console.error('Fejl ved hentning af kunder:', error);
-        res.status(500).send('Der opstod en fejl ved hentning af kunder.');
-    }
-});
 
-customerRouter.post('/Customer/Add', async (req, res) => {
-    try {
-        const { firstName, lastName, birthday, city } = req.body;
-        await controller.addCustomer({ firstName, lastName, birthday, city });
-        
-        res.redirect('/customers'); // Redirect til en oversigtsside eller anden relevant side
-    } catch (error) {
-        console.error('Fejl ved tilføjelse af kunde:', error);
-        res.status(500).send('Der opstod en fejl ved tilføjelse af kunde.');
-    }
-});
+
+
 
 customerRouter.get('/Customer/Edit/:id', async (req, res) => {
     try {
