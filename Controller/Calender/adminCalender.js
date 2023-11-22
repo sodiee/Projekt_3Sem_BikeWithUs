586b6b6adf42471c9234982064b1e07ffe214ae3
sodiee <mathi.carlsen@gmail.com>
2023-11-22 09:56:05 +0100

    kalender

--- Controller/Calender/adminCalender.js
+++ Controller/Calender/adminCalender.js
@@ -1,22 +1,27 @@
 let table = document.getElementById("table");
 //måned
+let months = ['january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december',]
 const localDate = new Date();
-let months = ['January',
-'February',
-'March',
-'April',
-'May',
-'June',
-'July',
-'August',
-'September',
-'October',
-'November',
-'December',]
 let currentMonth = months[localDate.getMonth()];
+let selectedMonth = currentMonth;
 
 //TR og TD
 for (let i = 1; i <= 31; i++) {
+    //Click current month
+    const rb = document.getElementById('choice-' + currentMonth + '');
+    rb.checked = true;
+
     //Laver objekter
     let trElement = document.createElement('tr');
     let tdElementDato = document.createElement('td');
@@ -25,7 +30,7 @@ for (let i = 1; i <= 31; i++) {
 
     //Tilfører værdier til elementer
     tdElementDato.id = i;
-    tdElementDato.textContent = i + '. ' + currentMonth;
+    tdElementDato.textContent = i + '. ' + selectedMonth;
     tdElementRejse.id = i + ': Rejse'
     tdElementRejse.textContent = '-'
     tdElementKunde.id = i + ': Kunde'
@@ -38,22 +43,32 @@ for (let i = 1; i <= 31; i++) {
     table.appendChild(trElement);
 }
 
-//Måneder-knap
-for (let i = 0; i < 12; i++) {
-    const btn = document.querySelector('#btn');
-          // handle button click
-          btn.onclick = function () {
-              const rbs = document.querySelectorAll('input[name="choice"]');
-              let selectedValue;
-              for (const rb of rbs) {
-                  if (rb.checked) {
-                      selectedValue = rb.value;
-                      break;
-                  }
-              }
-              alert(selectedValue);
-          };
+//const btn = document.getElementById('btn');
+let rbs = document.querySelectorAll('input[name="choice"]');
+for (const rb of rbs) {
+    rb.onclick = () => {
+        updateMonth();
+    };
 }
+
+
+function updateMonth() {
+    let selectedValue;
+
+    for (const rb of rbs) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+            selectedMonth = selectedValue;
+            break;
+        }
+    }
+
+    for (let i = 1; i <= 31; i++) {
+        let tdElementDato = document.getElementById(i);
+        tdElementDato.textContent = i + '. ' + selectedMonth;
+    }
+}
+
 
 // Tilføj denne funktion til at tilføje et event
 function addEvent(startDate, endDate, eventName/*skal ændres til selve journey*/) {
@@ -63,16 +78,15 @@ function addEvent(startDate, endDate, eventName/*skal ændres til selve journey*
         let tdElementKunde = document.getElementById(i + ': Kunde')
 
         if (tdElementRejse) {
-            tdElementRejse.textContent = ""
             // Opret et p-element for eventet og tilføj det til cellen
             let pElementRejse = document.createElement('p');
-            pElementRejse.textContent = eventName;
+            pElementRejse.textContent = eventName + ' - ';
             tdElementRejse.appendChild(pElementRejse);
         }
 
-        if(tdElementKunde) {
+        if (tdElementKunde) {
             tdElementKunde.textContent = ""
-         
+
             let pElementKunde = document.createElement('p');
             pElementKunde.textContent = 'Mikkel'; //journey.customer
             tdElementKunde.appendChild(pElementKunde);
@@ -82,3 +96,6 @@ function addEvent(startDate, endDate, eventName/*skal ændres til selve journey*
 
 addEvent(4, 4, "Mikkels fødselsdag");
 
+addEvent(4, 4, "Mathias' fødselsdag");
+
+knapValue();
