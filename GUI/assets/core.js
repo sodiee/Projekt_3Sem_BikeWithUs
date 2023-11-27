785af5c69fe8f290a714834f0e964942fa4537fe
sodiee <mathi.carlsen@gmail.com>
2023-11-27 23:35:04 +0100

    gui kalender

--- GUI/assets/core.js
+++ GUI/assets/core.js
@@ -245,10 +245,20 @@ function updateMonth() {
             break;
         }
     }
-
-    for (let i = 1; i <= 31; i++) {
+    clear();
+    for (let i = 1; i <= calculateDays(selectedMonthNumber); i++) {
         let tdElementDato = document.getElementById(i);
         tdElementDato.textContent = i + '. ' + months[selectedMonthNumber - 1];
     }
     getJourneys(months[selectedMonthNumber - 1]);
+}
+
+function calculateDays(month) {
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        return 31;
+    } else if (month == 2) {
+        return 28;
+    } else {
+        return 30;
+    }
 }
