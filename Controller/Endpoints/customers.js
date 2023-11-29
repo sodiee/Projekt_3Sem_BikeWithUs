07e6e454686ebed25463c3c044e2c3856ceef2d7
sb1004 <80931706+sb1004@users.noreply.github.com>
2023-11-29 08:57:46 +0100

    Ændringer til customers og pug

--- Controller/Endpoints/customers.js
+++ Controller/Endpoints/customers.js
@@ -131,5 +131,25 @@ customerRouter.get('/Mypage/:id', async (req, res) => {
     }
 });
 
+// CustomerPage
+
+customerRouter.get('/CustomerPage/:id', async (req, res) => {
+    // Check for login status using sessions or cookies
+    if (!req.session.isLoggedIn) {
+        try {
+            const customerId = req.params.id; 
+            const customerJourneys = await journeyController.getCustomerJourneys(customerId);
+            const customer = await controller.getCustomer(customerId);
+    
+            res.render('CustomerPage', { journeys: customerJourneys, customer: customer });
+        } catch (error) {
+            console.error('Fejl ved hentning af kundens side:', error);
+            res.status(500).send('Der opstod en fejl ved hentning af kundens side.');
+        }
+    } else {
+        res.redirect('/customerLogin');
+    }
+});
+
 
 export default customerRouter;
