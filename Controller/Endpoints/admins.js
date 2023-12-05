d3896389e19d59a023a9c28e1c271a78f42e0150
GloriousHyDra <lavrinenkom03@gmail.com>
2023-12-05 08:41:13 +0100

    updates

--- Controller/Endpoints/admins.js
+++ Controller/Endpoints/admins.js
@@ -46,7 +46,7 @@ adminRouter.get('/', (req, res) => {
     if (isAdminLoggedIn && adminUser) {
         res.render('adminMain', { knownUser: isAdminLoggedIn, adminUser: adminUser });
     } else {
-        res.redirect('/');
+        res.redirect('/admins/adminLogin');
     }
 });
 
